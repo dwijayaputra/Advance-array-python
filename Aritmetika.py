ba03053ba29f45557af566a5b1d1cfadad5da7ed
dwijayaputra <45097886+dwijayaputra@users.noreply.github.com>
2019-01-06 17:08:16 +0800

    Aritmetika python

---
+++ Aritmetika.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+# list python
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+
+# list array
+anp = np.array([1,2,3,4,5])
+bnp = np.array([6,7,8,9,10])
+
+hasil = a+b 
+hasil = anp+bnp
+hasil = anp-bnp
+hasil = anp*bnp
+hasil = anp/bnp
+hasil = anp**2
+
+print(hasil)
